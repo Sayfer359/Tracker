072710823f9e088f62eb86e2c1926346e942a493
Sayfer359 <sayfer359@gmail.com>
2026-01-16 23:27:44 +0300

    Update trecker2_ESP32_.ino

--- trecker2_ESP32_.ino
+++ trecker2_ESP32_.ino
@@ -1,23 +1,20 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-
-// Настройки
+
 #define BUZZER_PIN 13
 #define SERVICE_UUID "12345678-1234-1234-1234-123456789abc"
 #define CHARACTERISTIC_UUID "abcdef12-1234-1234-1234-123456789abc"
 #define BUTTON_CHARACTERISTIC_UUID "abcdef13-1234-1234-1234-123456789abc"
 
-// Объявление функций ДО их использования
 void triggerAlarm();
 void manualBuzzer();
 
 BLEServer *pServer;
 bool deviceConnected = false;
 bool alarmTriggered = false;
-bool alarmEnabled = true; // Флаг для включения/выключения тревоги
+bool alarmEnabled = true;
 
-// Класс для обработки событий подключения/отключения
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
@@ -29,19 +26,16 @@ class MyServerCallbacks: public BLEServerCallbacks {
       deviceConnected = false;
       Serial.println("Device Disconnected");
       
-      // Тревога срабатывает ТОЛЬКО если включена
       if (alarmEnabled) {
         triggerAlarm();
       } else {
         Serial.println("Alarm disabled - no trigger on disconnect");
       }
-      
-      // Перезапускаем рекламу
+      
       BLEDevice::startAdvertising();
     }
 };
-
-// Класс для обработки команд от приложения
+
 class ButtonCharacteristicCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       String value = pCharacteristic->getValue();
@@ -60,13 +54,11 @@ class ButtonCharacteristicCallbacks: public BLECharacteristicCallbacks {
           digitalWrite(BUZZER_PIN, LOW);
           alarmTriggered = false;
         }
-        else if (command == '2') {
-          // Включить тревогу при отключении
+        else if (command == '2') {
           alarmEnabled = true;
           Serial.println("Alarm ENABLED - will trigger on disconnect");
         }
-        else if (command == '3') {
-          // Выключить тревогу при отключении
+        else if (command == '3') {
           alarmEnabled = false;
           Serial.println("Alarm DISABLED - will NOT trigger on disconnect");
         }
@@ -79,34 +71,28 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW);
 
-  // Создаем BLE-устройство
   BLEDevice::init("Tracker");
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Создаем BLE-сервис
+
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  
-  // Создаем характеристику для данных
+  
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  // Создаем характеристику для кнопки
   BLECharacteristic *pButtonCharacteristic = pService->createCharacteristic(
                                          BUTTON_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
   
   pButtonCharacteristic->setCallbacks(new ButtonCharacteristicCallbacks());
-  
-  // Начальное значение характеристики
+ 
   pCharacteristic->setValue("Tracker Ready");
   pService->start();
-
-  // Настройка рекламирования
+
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
@@ -120,12 +106,10 @@ void setup() {
 }
 
 void loop() {
-  if (deviceConnected) {
-    // Периодическая проверка соединения
+  if (deviceConnected) { 
     delay(1000);
   }
-  
-  // Обработка команд из Serial для тестирования
+  
   if (Serial.available()) {
     char command = Serial.read();
     if (command == '1') {
@@ -152,16 +136,15 @@ void loop() {
     }
   }
 }
-
-// Реализация функций ПОСЛЕ их объявления
+
 void triggerAlarm() {
-  if (alarmTriggered) return; // Уже в режиме тревоги
+  if (alarmTriggered) return;
   
   Serial.println("ALARM! Device is out of range!");
   alarmTriggered = true;
   
   for (int i = 0; i < 10; i++) {
-    if (!alarmTriggered) break; // Проверка отмены тревоги
+    if (!alarmTriggered) break;
     digitalWrite(BUZZER_PIN, HIGH);
     delay(200);
     digitalWrite(BUZZER_PIN, LOW);
